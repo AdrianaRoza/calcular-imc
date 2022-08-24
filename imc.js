2823f37ac2d2c3af3e6d63bb343fcc8da26c9692
AdrianaRoza <adriana.roza.aka@gmail.com>
2022-08-24 19:20:08 -0400

    refactor

--- imc.js
+++ imc.js
@@ -9,9 +9,32 @@ function imc () {
   if (nome !== '' && altura !== '' && peso !== '') {
     
     const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+    let classificacao = '';
     
-    resultado.textContent = valorIMC;
+    if (valorIMC < 16.9){
+      classificacao = 'Muito abaixo do peso.';
+  }else if (valorIMC < 18.4) {
+      classificacao = 'Abaixo do peso.';
+  }else if (valorIMC < 24.9) {
+      classificacao = 'Peso normal.';
+  }else if (valorIMC < 29.9){
+      classificacao = 'Acima do peso.';
+  }else if (valorIMC < 34.9){
+      classificacao = 'Com obesidade grau I.';
+  }else if (valorIMC < 40){
+      classificacao = 'Com obesidade grau II';
+  }else {
+      classificacao = 'Com obesidade grau III. Cuidado!!';
   }
+
+          resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
+        
+    }else {
+        resultado.textContent = 'Preencha todos os campos!!!';
+    }
+
 }
 
-calcular.addEventListener('click',imc);
+calcular.addEventListener('click', imc); 
+
